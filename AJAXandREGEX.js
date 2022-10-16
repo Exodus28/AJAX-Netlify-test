55e6854a3c7879b1e043adcfcddb18a8d7de4301
Chris <43829555+Exodus28@users.noreply.github.com>
2022-10-16 20:20:21 +0100

    Add files via upload

--- AJAXandREGEX.js
+++ AJAXandREGEX.js
@@ -1,3 +1,11 @@
 $.get("test.html", function(data) { //get html file, callback function, data is contents of file
     alert(data)
 })
+
+// .ajax is preferential to .get as it gives more control e.g. can set a 'fail' method to alert user to failed tas
+
+$.ajax({
+    url:"test.html"
+}).done(function(data) {
+    alert(data)
+})
